3a704f9ab557d3f1aeb7a0b043b948770eb357b1
Ranveer Chaudhary <64607338+ranveerchaudhary@users.noreply.github.com>
2025-02-22 15:37:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def quenchflow(USER_KEY, API_TOKEN):
 })
 
 if len(sys.argv) == 3:
-    quenchflow(sys.argv[2], sys.argv[3])
+    quenchflow(sys.argv[1], sys.argv[2])
 else:
     print("Invalid arg length :", len(sys.argv))
     print("Script failed")
