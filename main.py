30f4b92b9c007f93df49c9ef3701994f857e3b98
Ranveer Chaudhary <64607338+ranveerchaudhary@users.noreply.github.com>
2025-02-22 15:07:00 +0530

    Defined main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import sys
+import requests
 
+def quenchflow(USER_KEY, API_TOKEN):
+    requests.post("https://api.pushover.net/1/messages.json", data={
+    "token":  API_TOKEN,
+    "user": USER_KEY,
+    "message": "quenchflow triggered"
+})
+
+if len(sys.argv) == 3:
+    quenchflow(sys.argv[2], sys.argv[3])
+else:
+    print("Invalid arg length :", len(sys.argv))
+    print("Script failed")
